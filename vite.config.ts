15f52a8568b623cd724c9810f2ed32ff38fe38a5
Guilherme Bomfim <guigui_bsg@hotmail.com>
2025-08-31 01:30:54 -0300

    deploy com o yml

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/VBDesign",
+  base: "/VBDesign/",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
