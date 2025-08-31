ad5d1c7cc35fda6d058fa92effa3d931d3201e7e
Guilherme Bomfim <guigui_bsg@hotmail.com>
2025-08-31 00:31:55 -0300

    Ajustes para deploys no github

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,6 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
-  optimizeDeps: {
-    entries: ["src/main.tsx", "src/tempobook/**/*"],
-  },
-  plugins: [
-    react(),
-    tempo(),
-  ],
-  resolve: {
-    preserveSymlinks: true,
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    // @ts-ignore
-    allowedHosts: true,
-  }
+  plugins: [react()],
+  base: "/VBDesign",
 });
